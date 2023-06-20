601de3a81e38f18655a659bf482117901448cabe
btan1 <btan1@cmcglobal.vn>
2023-06-20 13:22:25 +0700

    add main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.61.0"
+    }
+  }
+}
+
+#configuration provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
