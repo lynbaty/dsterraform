7ad272a29d2f8913cbe36249f0d2d2620eca0b13
btan1 <btan1@cmcglobal.vn>
2023-06-20 16:34:31 +0700

    test authen

--- main.tf
+++ main.tf
@@ -10,10 +10,12 @@ terraform {
 #configuration provider
 provider "azurerm" {
   features {}
+  tenant_id       = "e3dcec5f-1b37-48ee-a07f-8bdbd2646b5c"
+  subscription_id = "0e09aad1-0711-40d5-be06-c78a8fd5fccf"
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources-2"
   location = "West Europe"
 }
